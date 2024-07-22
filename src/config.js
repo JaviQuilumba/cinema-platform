a55d9645b1f5d9584785c6907a0edbca445b1be7
JaviQuilumba <javier0611quilumba@gmail.com>
2024-07-21 20:26:04 -0500

    test2

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 const config = {
-    BASE_URL_MOVIES: 'http://lb-apigateway-1412267724.us-east-2.elb.amazonaws.com/gateway/api/movies',
-    BASE_URL_UPDATE: 'http://lb-apigateway-1412267724.us-east-2.elb.amazonaws.com/gateway/api/movies',
-    BASE_URL_DELETE: 'http://lb-apigateway-1412267724.us-east-2.elb.amazonaws.com/gateway/api/movies',
-    BASE_URL_CREATEMOVIE: 'http://lb-apigateway-1412267724.us-east-2.elb.amazonaws.com/gateway/api/movies',
-    BASE_URL_AUTH: 'http://lb-apigateway-1412267724.us-east-2.elb.amazonaws.com/gateway/api/auth/login',
+    BASE_URL_MOVIES: 'http://lb-g14-microservices-107665025.us-east-2.elb.amazonaws.com/api/movies',
+    BASE_URL_UPDATE: 'http://lb-g14-microservices-107665025.us-east-2.elb.amazonaws.com/api/movies',
+    BASE_URL_DELETE: 'http://lb-g14-microservices-107665025.us-east-2.elb.amazonaws.com/api/movies',
+    BASE_URL_CREATEMOVIE: 'http://lb-g14-microservices-107665025.us-east-2.elb.amazonaws.com/api/movies',
+    BASE_URL_AUTH: 'http://lb-g14-microservices-107665025.us-east-2.elb.amazonaws.com/api/auth/login',
 
   };
   //
