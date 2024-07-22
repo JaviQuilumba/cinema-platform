7263d9c7a8762456365d7b4f01904f314e385f46
JaviQuilumba <javier0611quilumba@gmail.com>
2024-07-22 02:59:57 -0500

    update files 5

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ const config = {
     BASE_URL_OFFERS: 'http://lb-offers-1788351580.us-east-2.elb.amazonaws.com/api/offers',
     BASE_URL_ROOMS: 'http://lb-rooms-types-773377645.us-east-2.elb.amazonaws.com/api/rooms',
     BASE_URL_SNACKC: 'http://lb-create-snacks-292109628.us-east-2.elb.amazonaws.com/api/snacks',
-    BASE_URL_SNACKL: 'http://localhost:3012/api/snacks',
+    BASE_URL_SNACKL: 'http://lb-list-snacks-215569271.us-east-2.elb.amazonaws.com/api/snacks',
     BASE_URL_SNACKU: 'http://localhost:3013/api/snacks',
     BASE_URL_SNACKD: 'http://localhost:3014/api/snacks',
     BASE_URL_CREATEROOMS: 'http://lb-create-rooms-360853064.us-east-2.elb.amazonaws.com/api/cinemas',
